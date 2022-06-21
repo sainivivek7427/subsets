3cceded6f21663d16a24aac42ffdbd99491a5219
Vivek Saini <93831086+sainivivek7427@users.noreply.github.com>
2022-06-21 08:38:13 +0000

    create main func

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
-
+#include "bits/stdc++.h"
+using namespace std;
 int main() {
-  std::cout << "Hello World!\n";
+  cout<<"enter the size: ";
+  int n;
+  cin>>n;
+  int *arr=new int[n];
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+  int *outp
+  subset(arr,n);
+  
 }
